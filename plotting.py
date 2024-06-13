2a14ccb744155b4dd4bc0b168983654a17cf1734
Noa Second <contact@noasecond.com>
2024-06-13 14:50:31 +0200

    :memo: Add documentation

--- plotting.py
+++ plotting.py
@@ -5,6 +5,17 @@ import pyqtgraph as pg
 
 class PicoPlotter(QtWidgets.QMainWindow):
     def __init__(self, channels, title, parent):
+        """
+        Initialize the PlottingWidget.
+
+        Args:
+            channels (list): A list of channels.
+            title (str): The title of the widget.
+            parent (QWidget): The parent widget.
+
+        Returns:
+            None
+        """
         super().__init__(parent)
         self.channels = channels
         self.title = title
@@ -13,6 +24,15 @@ class PicoPlotter(QtWidgets.QMainWindow):
         self.data = [[] for _ in channels]
 
     def initUI(self, parent):
+        """
+        Initializes the user interface for the plot.
+
+        Args:
+            parent: The parent widget.
+
+        Returns:
+            None
+        """
         layout = QtWidgets.QVBoxLayout(parent)
         
         self.plotWidget = pg.PlotWidget(title=self.title, parent=parent)
@@ -25,6 +45,16 @@ class PicoPlotter(QtWidgets.QMainWindow):
         self.timer.start(25)  # Frequency in ms
 
     def update_plot(self):
+        """
+        Update the plot with new data.
+
+        This method fetches data from the specified channels and updates the plot with the new values.
+        It appends the fetched values to the existing data and sets the curves to display the updated data.
+
+        Raises:
+            Exception: If there is an error fetching the data.
+
+        """
         try:
             values = [pico.get_value(channel) for channel in self.channels]
             for i, value in enumerate(values):
